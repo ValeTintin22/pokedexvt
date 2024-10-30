5ab0183f0d4fc80617b07b86567af57acdacd82b
ctintin97 <ctintin97@gmail.com>
2024-10-30 04:26:39 +0000

    first commit

---
+++ src/app/Components/pokemon-view/pokemon-view.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { GraficoComponent } from '../grafico/grafico.component';
+import { PokemonComponent } from '../pokemon/pokemon.component';
+
+@Component({
+  selector: 'app-pokemon-view',
+  standalone: true,
+  imports: [PokemonComponent,GraficoComponent],
+  templateUrl: './pokemon-view.component.html',
+  styleUrl: './pokemon-view.component.css'
+})
+export class PokemonViewComponent {
+pokemonImagen: string= 'https://raw.githubusercontent.com/PokeAPI/sprites/6e5b8ac354ddc347104840cbd14ad6e0b2fdb551/sprites/pokemon/5.png'
+pokemonId: number=0;
+
+handlePokemon(event:number){
+  this.pokemonId += event
+}
+
+siguientePokemon():void {
+  this.pokemonId++;
+  this.pokemonImagen = 'https://raw.githubusercontent.com/PokeAPI/sprites/6e5b8ac354ddc347104840cbd14ad6e0b2fdb551/sprites/pokemon/'+this.pokemonId+'.png';
+}
+}
+
