5c127cf98536113023a627216f40261f2530124d
ctintin97 <ctintin97@gmail.com>
2025-03-24 20:25:02 +0000

    final

--- src/app/pokedex/pokedex.component.ts
+++ src/app/pokedex/pokedex.component.ts
@@ -1,23 +1,31 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
+  import { Component } from '@angular/core';// Suggested code may be subject to a license. Learn more: ~LicenseLog:3436635119.
   selector: 'app-pokedex',
-  standalone: true,
-  imports: [],
   templateUrl: './pokedex.component.html',
-  styleUrl: './pokedex.component.css'
+  styleUrls: ['./pokedex.component.css']
 })
 export class PokedexComponent {
-  @Input() pokemons: string[] = []; 
-  pokedex: string[] = []; 
+  pokemonList: any[] = []; // Lista inicializada como arreglo vacío
 
-  mostrarListaAleatoria() {
-    this.pokedex = []; 
-    let cantidad = 5; 
-    for (let i = 0; i < cantidad; i++) {
-      let indiceAleatorio = Math.floor(Math.random() * this.pokemons.length);
-      this.pokedex.push(this.pokemons[indiceAleatorio]);
-    }
+  mostrarListaAleatoria(): void {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+      .then(response => response.json())
+      .then(data => {
+        const pokemons = data.results.map((pokemon: any) => pokemon.name);
+        this.pokemonList = this.obtenerPokemonesAleatorios(pokemons, 3);
+      })
+      .catch(error => {
+        console.error('Error al obtener los Pokémon:', error);
+        // Manejar el error de manera apropiada (mostrar mensaje de error, etc.)
+      });
+  }
+
+  obtenerPokemonesAleatorios(pokemones: string[], cantidad: number): string[] {
+    const aleatorios = [...pokemones].sort(() => Math.random() - 0.5);
+    return aleatorios.slice(0, cantidad);
   }
 }
 
+  
